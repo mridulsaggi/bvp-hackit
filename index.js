e18b0eb63b23da5bf59a814c1eff938f9d3db7b6
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2023-03-28 13:28:27 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const fs=require("fs");
+const path=require("path");
+const app=express();
+const port=3000;
+// console.log(path.join(__dirname))
+app.use(express.static(path.join(__dirname)));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'));
+});
+app.get("/quiz",(req,res)=>{
+    res.sendFile(path.join(__dirname,'quiz.html'));
+});
+app.listen(port,()=>{
+    console.log("app running successfully on port %d",port);
+});
