d4dfc83fa4d6633e2d0aadf77519a0e0beefaab1
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2023-03-31 03:25:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.use(express.static(path.join(__dirname)));
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 });
-app.get("bvp-hackit/quiz",(req,res)=>{
+app.get("/bvp-hackit/quiz",(req,res)=>{
     res.sendFile(path.join(__dirname,'quiz.html'));
 });
-app.get("bvp-hackit/videolink",(req,res)=>{
+app.get("/bvp-hackit/videolink",(req,res)=>{
     res.sendFile(path.join(__dirname,'index3.html'));
+});
+app.get("/bvp-hackit/workshops",(req,res)=>{
+    res.sendFile(path.join(__dirname,'index5.html'));
 });
 app.listen(port,()=>{
     console.log("app running successfully on port %d",port);
